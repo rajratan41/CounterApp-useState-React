737a2ef1d00ea7be14571b92c770835851050be1
Raj Ratan <raj808341@gmail.com>
2023-05-03 14:04:20 +0530

    Restrict Counter to increase till 10 and decrease till 0

--- state-counter/src/App.js
+++ state-counter/src/App.js
@@ -11,8 +11,12 @@ const App = () => {
       </header>
       <h2>Current value of Count is {count}</h2>
       <button onClick={() => setCount(0)}>Reset Count</button>
-      <button onClick={() => setCount(count + 1)}>Increase Count</button>
-      <button onClick={() => setCount(count - 1)}>Decrease Count</button>
+      <button onClick={() => (count >= 10 ? "" : setCount(count + 1))}>
+        Increase Count
+      </button>
+      <button onClick={() => (count <= 0 ? "" : setCount(count - 1))}>
+        Decrease Count
+      </button>
     </div>
   );
 };
