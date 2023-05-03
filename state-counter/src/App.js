aea8f38c828b65b0d7ed00b3d6efb363d43a4041
Raj Ratan <raj808341@gmail.com>
2023-05-03 13:51:18 +0530

    Created Counter App using useState hook

--- state-counter/src/App.js
+++ state-counter/src/App.js
@@ -1,15 +1,20 @@
-import React from "react"
-import "./App.css"
+import React, { useState } from "react";
+import "./App.css";
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
       <header>
-        <h2>Counter App using useState</h2>
+        <h1>Counter App using useState</h1>
       </header>
-
+      <h2>Current value of Count is {count}</h2>
+      <button onClick={() => setCount(0)}>Reset Count</button>
+      <button onClick={() => setCount(count + 1)}>Increase Count</button>
+      <button onClick={() => setCount(count - 1)}>Decrease Count</button>
     </div>
-  )
-}
+  );
+};
 
 export default App;
