00fbf6f026a85dedcd923f37c8b241de69c6a47e
Raj Ratan <110181177+rajratan41@users.noreply.github.com>
2023-05-03 08:07:53 +0000

    added header to app

---
+++ state-counter/src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "./App.css"
+
+const App = () => {
+  return (
+    <div className="App">
+      <header>
+        <h2>Counter App using useState</h2>
+      </header>
+
+    </div>
+  )
+}
+
+export default App;
